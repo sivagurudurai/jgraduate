e290bf2c7d5a3f4d104a189db3ba17a42184cbaa
codedread <codedread@859a532a-5c49-11de-992f-fdd7e25b9163>
2009-06-20 18:02:11 +0000

    Rename script to conform for convention.  Very start of test page and script (still no functionality yet)

---
+++ trunk/jquery.jgraduate.js
@@ -0,0 +1,23 @@
+﻿/*
+ * jGraduate 0.01
+ *
+ * jQuery Plugin for a gradient picker
+ *
+ * Copyright (c) 2009 Jeff Schiller
+ * http://blog.codedread.com/
+ *
+ * Apache 2 License
+ *
+ */
+jQuery.fn.jGraduate =
+	function(options) {
+		return this.each( function() {
+			var $this = $(this), id = $this.attr('id');
+            if (!id)
+            {
+              alert('Container element must have an id attribute to maintain unique id strings for sub-elements.');
+              return;
+            }		
+			alert('yo');
+		});
+	};
