993ff3f1f35ada89a34d9533511a7c2557a55432
codedread <codedread@859a532a-5c49-11de-992f-fdd7e25b9163>
2009-06-21 22:07:49 +0000

    Get ok/cancel callbacks working to hide picker.  Add input boxes for x,y,color,opacity - still no real functionality yet

--- trunk/jquery.jgraduate.js
+++ trunk/jquery.jgraduate.js
@@ -12,6 +12,8 @@
 ns = { svg: 'http://www.w3.org/2000/svg' };
 jQuery.fn.jGraduate =
 	function(options) {
+	 	var $arguments = arguments;
+	 	console.log($arguments);
 		return this.each( function() {
 			var $this = $(this), id = $this.attr('id');
             if (!id)
@@ -19,11 +21,42 @@ jQuery.fn.jGraduate =
               alert('Container element must have an id attribute to maintain unique id strings for sub-elements.');
               return;
             }
+            
+            var 
+            okClicked = function() {
+            	$.isFunction($this.okCallback) && $this.okCallback();
+            	$this.hide();
+            },
+            cancelClicked = function() {
+            	$this.gradient = null;
+            	$.isFunction($this.cancelCallback) && $this.cancelCallback();
+            	$this.hide();
+            };
+
+            $.extend(true, $this, // public properties, methods, and callbacks
+              {
+                gradient: null,
+                okCallback: $.isFunction($arguments[0]) && $arguments[0] || null, // commitCallback function can be overridden to return the selected color to a method you specify when the user clicks "OK"
+                cancelCallback: $.isFunction($arguments[1]) && $arguments[1] || null, // cancelCallback function can be overridden to a method you specify when the user clicks "Cancel"
+              });
+            
             $this.addClass('jGraduate_Picker');
             $this.html('<div id="' + id + '_jGraduate_Swatch" class="jGraduate_Swatch"></div><div id="' + id + 
-            	'_jGraduate_Form" class="jGraduate_Form"><input type="button" id="' + id + 
+            	'_jGraduate_Form" class="jGraduate_Form">Begin:<br/><label>x</label><input type="text" name="' + id + 
+            	'_jGraduate_x1" size="3"/><label>y</label><input type="text" name="' + id +
+            	'_jGraduate_y1" size="3"/><br/><label>Color</label><input type="text" name="' + id +
+            	'_jGraduate_beginColor" size="6"/><br/><label>Opacity</label><input type="text" name="' + id +
+            	'_jGraduate_beginOpacity" size="4"/><br/>End:<br/><label>x</label><input type="text" name="' + id + 
+            	'_jGraduate_x2" size="3"/><label>y</label><input type="text" name="' + id +
+            	'_jGraduate_y2" size="3"/><br/><label>Color</label><input type="text" name="' + id +
+            	'_jGraduate_endColor" size="6"/><br/><label>Opacity</label><input type="text" name="' + id +
+            	'_jGraduate_endOpacity" size="4"/><br/><div class="jGraduate_OkCancel"><input type="button" id="' + id + 
             	'_jGraduate_Ok" class="jGraduate_Ok" value="OK"/><input type="button" id="' + id +
-            	'_jGraduate_Cancel" class="jGraduate_Cancel" value="Cancel"/></div>').show();
+            	'_jGraduate_Cancel" class="jGraduate_Cancel" value="Cancel"/></div></div>').show();
+            var okButton = $('#'+id+'_jGraduate_Ok');
+            okButton.bind('click', okClicked);
+            var cancelButton = $('#'+id+'_jGraduate_Cancel');
+            cancelButton.bind('click', cancelClicked);
             
             // Set up all the SVG elements (the gradient, stops and rectangle)
             var container = document.getElementById(id+'_jGraduate_Swatch');
@@ -40,14 +73,15 @@ jQuery.fn.jGraduate =
 			grad.setAttribute('y1','0.0');
 			grad.setAttribute('x2','1.0');
 			grad.setAttribute('y2','1.0');
+			$this.gradient = grad;
 			
 			var begin = grad.appendChild(document.createElementNS(ns.svg, 'stop'));
 			begin.setAttribute('offset', '0.0');
-			begin.setAttribute('stop-color', 'black');
+			begin.setAttribute('stop-color', 'red');
 
 			var end = grad.appendChild(document.createElementNS(ns.svg, 'stop'));
 			end.setAttribute('offset', '1.0');
-			end.setAttribute('stop-color', 'white');
+			end.setAttribute('stop-color', 'yellow');
 			
             var rect = svg.appendChild(document.createElementNS(ns.svg, 'rect'));
             rect.id = 'jgraduate_rect';
